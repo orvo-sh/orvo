1996aff91811f13eb2d725c5b390bd8119996294
d3cie <decefemz@gmail.com>
2026-06-20 03:09:24 +0200

    chore: remove unused docs and web apps

--- apps/docs/src/routes/+page.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-	import { Button } from '@repo/components/ui/button';
-</script>
-
-<h1>Docs</h1>
-<Button>Browse Components</Button>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
--- apps/web/src/routes/+page.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-	import { Button } from '@repo/components/ui/button';
-</script>
-
-<h1>Web</h1>
-<Button>Open Components</Button>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
